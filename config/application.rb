242d22a94d1e6ffaa198b1b7eab4d33df382dbe3
Duy <Duyso1000@gmail.com>
2016-11-17 15:35:20 +0700

    test4

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 module Web
   class Application < Rails::Application
@@ -23,3 +24,4 @@ module Web
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.secret_key = '48e200e27d50aad998544f2d5dd356922c9281833c7fd547e72ac3445176552d9fd571c726c11661c9068b712d21039ac00ff46ccc2c2e87cb924cc92004970f'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
