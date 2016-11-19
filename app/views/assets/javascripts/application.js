4b4301bf7711368a5957a9f434172057af9cde86
Duy <Duyso1000@gmail.com>
2016-11-19 16:27:10 +0700

    social

---
+++ app/views/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require slick.min.js
+//= require bootstrap.min.js
+//= require html5shiv.min.js
+//= require wow.min.js
+//= require custom.js
+//= require jquery.fancybox.pack.js
+//= require jquery.newsTicker.min.js
+//= require jquery.min.js
+//= require jquery.li-scroller.1.0.js
+//= require respond.min.js
+
+//= require_tree  .
